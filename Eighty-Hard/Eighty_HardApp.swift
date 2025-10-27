f46e20cae38e37cb34c99b9d103d17b9b11ab5f7
Jonathan Young <jonathanyoung7718@gmail.com>
2025-10-27 17:46:17 -0500

    Initial Commit

---
+++ Eighty-Hard/Eighty_HardApp.swift
@@ -0,0 +1,17 @@
+//
+//  Eighty_HardApp.swift
+//  Eighty-Hard
+//
+//  Created by Jonathan Young on 10/27/25.
+//
+
+import SwiftUI
+
+@main
+struct Eighty_HardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
